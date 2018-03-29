5d7e04a37edcfd8d59d91a34e23659b080057640
Jarid11 <Marinos1995@gmail.com>
2018-03-29 17:51:52 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const massive = require("massive");
+require("dotenv").config();
+
+const ctrl = require("./controller.js");
 
 const app = express();
-app.use( bodyParser.json() );
-app.use( cors() );
 
-const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
+massive(process.env.CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set("db", dbInstance);
 
+    // dbInstance
+    //   .new_planes()
+    //   .then(planes => console.log(planes))
+    //   .catch(err => console.log(err));
+    // dbInstance
+    //   .get_planes()
+    //   .then(planes => console.log(planes))
+    //   .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
+
+app.get("/api/planes", ctrl.getPlanes);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
